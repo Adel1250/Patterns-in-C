c1d4f90ce79b41e5fad3d5d2d63197c6b992bc7b
Adel1250 <adelashrafhamouda@gmail.com>
2023-01-28 20:05:06 +0200

    Triangle 2

---
+++ triangle_2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
